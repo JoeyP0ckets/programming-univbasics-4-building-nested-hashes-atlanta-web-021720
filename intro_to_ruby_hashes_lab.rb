38ba7325c5e9b02f87746db5308a0df0b6d0eb69
Joseph Patterson <thinkitmakeituseit@gmail.com>
2020-02-13 23:14:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,11 +1,9 @@
 def base_hash {
   monopoly = {
-    :railroads 
+    :railroads => {}
   }
-  
-  
-}
-  
+  base_hash
+ end 
   #describe "the base_hash method returns a Hash that" do
     #it "has one key" do
       #expect(base_hash.keys.count).to(eq(1), "Make sure you have at least one key defined in the monopoly hash")
@@ -13,7 +11,7 @@ def base_hash {
 
     #it "the one key is called :railroads" do
       #expect(base_hash.has_key?(:railroads)).to(be_truthy, "Make sure you have a key called :railroads in the `monopoly` Hash and that :railroads points to a Hash")
-    end
+ 
 
     it "the one key, :railroads, points to an empty Hash" do
       expect(base_hash[:railroads]).to eq({})
