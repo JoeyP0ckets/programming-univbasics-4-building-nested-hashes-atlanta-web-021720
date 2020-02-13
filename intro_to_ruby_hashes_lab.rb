7db25449bd950bc5c697c8046d2a5e91d0d2c147
Joseph Patterson <thinkitmakeituseit@gmail.com>
2020-02-13 23:12:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,27 @@
-def base_hash
+def base_hash {
+  monopoly = {
+    :railroads 
+  }
+  
+  
+}
+  
+  #describe "the base_hash method returns a Hash that" do
+    #it "has one key" do
+      #expect(base_hash.keys.count).to(eq(1), "Make sure you have at least one key defined in the monopoly hash")
+    #end
+
+    #it "the one key is called :railroads" do
+      #expect(base_hash.has_key?(:railroads)).to(be_truthy, "Make sure you have a key called :railroads in the `monopoly` Hash and that :railroads points to a Hash")
+    end
+
+    it "the one key, :railroads, points to an empty Hash" do
+      expect(base_hash[:railroads]).to eq({})
+    end
+  end
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+	
 end
 
 def monopoly_with_second_tier
