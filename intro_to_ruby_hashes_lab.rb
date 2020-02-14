dfca77e336bde121328ac00f13423015723a1a2a
Joseph Patterson <thinkitmakeituseit@gmail.com>
2020-02-14 00:10:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,10 +33,9 @@ def monopoly_with_third_tier
       :reading_railroad => {},
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
-     :x_railroad => {},
+      :shortline_railroad => {},
       },
-      
-    }
+      }
   }
   monopoly
  
