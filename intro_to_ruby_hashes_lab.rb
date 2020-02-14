64387f6fe4a20b96434fac0b694257978b934ed2
Joseph Patterson <thinkitmakeituseit@gmail.com>
2020-02-14 00:47:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,11 +54,11 @@ def monopoly_with_fourth_tier
       }
   }
   
-  print("HELLO JOEY")
-  print("HELLO JOEY")  
-  print("HELLO JOEY")
-  print(monopoly[:railroads][:names])
-  print("HELLO JOEY")
+  printf("HELLO JOEY")
+  printf("HELLO JOEY")  
+  printf("HELLO JOEY")
+  printf(monopoly[:railroads][:names])
+  printf("HELLO JOEY")
   monopoly
  end
  
