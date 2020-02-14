7725585372fe1cd85f1844c4403b2312ade30cd0
Joseph Patterson <thinkitmakeituseit@gmail.com>
2020-02-14 00:40:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,7 +46,7 @@ def monopoly_with_fourth_tier
        four_pieces_owned: 200
       },
     :names => {
-      :reading_railroad => {joey_patterson: "$100"},
+      :reading_railroad => {mortgage value: "$100"},
       :pennsylvania_railroad => {mortgage_value: "2"},
       :b_and_o_railroad => {mortgage_value: "3"},
       :shortline_railroad => {mortgage_value: "4"}
